0a9ce427655f2d43fd49bd5da4da3117fbe0e903
hagimoto0943 <hagi.san.09275571@gmail.com>
2025-11-25 15:57:29 +0900

    fixed s3 module project_name

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -20,14 +20,14 @@ module "k3s-cluster" {
 
   key_name = "notree-key"
 
-  k3s_token = "K10eaa54c658f00007744de25624a3760c35fca7dd577f39f1b864e7135f1c9af2b::server:69dbbc28387b18dbca590d3ef97c2c33"
+  k3s_token     = "K10eaa54c658f00007744de25624a3760c35fca7dd577f39f1b864e7135f1c9af2b::server:69dbbc28387b18dbca590d3ef97c2c33"
   s3_bucket_arn = module.s3.bucket_arn
 }
 
 module "s3" {
   source = "../../modules/s3"
 
-  project_name = "cpa-app"
+  project_name = "notree"
   env          = "dev"
 }
 
